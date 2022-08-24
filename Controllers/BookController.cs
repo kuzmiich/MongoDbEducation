2ebce67d5a331bbe8f07a79e2115eaa604954deb
240Dell <240Dell@IVANK>
2022-08-24 11:41:32 +0300

    Init book store

---
+++ Controllers/BookController.cs
@@ -0,0 +1,75 @@
+﻿using Microservices.BookStoreApi.Models;
+using Microservices.BookStoreApi.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Microservices.BookStoreApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BookController : ControllerBase
+    {
+        private readonly BookService _bookService;
+
+        public BookController(BookService bookService) =>
+            _bookService = bookService;
+
+        [HttpGet]
+        public async Task<List<Book>> GetAll() =>
+            await _bookService.GetAllAsync();
+
+        [HttpGet("{id:length(24)}")]
+        public async Task<ActionResult<Book>> Get(string id)
+        {
+            var book = await _bookService.GetAsync(id);
+
+            if (book is null)
+            {
+                return NotFound();
+            }
+
+            return book;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(Book newBook)
+        {
+            await _bookService.CreateAsync(newBook);
+
+            return CreatedAtAction(nameof(Get), new { id = newBook.Id }, newBook);
+        }
+
+        [HttpPut("{id:length(24)}")]
+        public async Task<IActionResult> Update(string id, Book updatedBook)
+        {
+            var book = await _bookService.GetAsync(id);
+
+            if (book is null)
+            {
+                return NotFound();
+            }
+
+            updatedBook.Id = book.Id;
+
+            await _bookService.UpdateAsync(id, updatedBook);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id:length(24)}")]
+        public async Task<IActionResult> Delete(string id)
+        {
+            var book = await _bookService.GetAsync(id);
+
+            if (book is null)
+            {
+                return NotFound();
+            }
+
+            await _bookService.RemoveAsync(id);
+
+            return NoContent();
+        }
+    }
+}
